3022b2ae5d5c412d19363bbc6d908d413979f665
Marius Reimer <reime005@gmail.com>
2018-08-09 23:58:53 +0200

    contract deployed to rinkeby testnet

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -1,5 +1,9 @@
 require('babel-register');
 require('babel-polyfill');
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var infura_apikey = "...";
+var mnemonic = "...";
+
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,6 +13,10 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      network_id: 4,
+      provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/" + infura_apikey, 0),
     }
   }
 };
